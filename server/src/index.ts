61ae9714515baced4f2c85be82fac01ff99816a4
NionGeraghty <geraghnb@tcd.ie>
2026-07-04 12:51:07 +0100

    Add food type and get command

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import { Food } from './types';
 
 const app = express();
 const PORT = 4000;
+app.use(express.json());
+
+const foods: Food[] = [
+  {
+    id: '1',
+    name: 'Chicken breast',
+    caloriesPer100g: 165,
+    proteinPer100g: 31,
+    carbsPer100g: 0,
+    fatPer100g: 3.6,
+    fibrePer100g: 0,
+  },
+];
 
 app.get('/', (req, res) => {
   res.send('Nutrition tracker API is running');
+});
+
+app.get('/foods', (req, res) => {
+  res.json(foods);
 });
 
 app.listen(PORT, () => {
---
+++ server/src/types.ts
@@ -0,0 +1,9 @@
+export interface Food {
+  id: string;
+  name: string;
+  caloriesPer100g: number;
+  proteinPer100g: number;
+  carbsPer100g: number;
+  fatPer100g: number;
+  fibrePer100g: number;
+}
